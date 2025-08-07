795a1f0280175fa179f13d99d5c6f5f2dfd80bc3
rockberpro <rockberpro@gmail.com>
2025-08-06 22:42:12 -0300

    chore: Setup PWA and Primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,29 @@
+import Lara from '@primeuix/themes/lara';
+import { definePreset } from '@primeuix/themes';
+
+// Using the Tailwind CSS color palette
+// @source: https://primevue.org/theming/styled/
+const LaraMountainMeadow = definePreset(Lara, {
+  semantic: {
+    colorScheme: {
+      primary: {
+          0: '{teal.0}',
+         50: '{teal.50}',
+        100: '{teal.100}',
+        200: '{teal.200}',
+        300: '{teal.300}',
+        400: '{teal.400}',
+        500: '{teal.500}',
+        600: '{teal.600}',
+        700: '{teal.700}',
+        800: '{teal.800}',
+        900: '{teal.900}',
+        950: '{teal.950}'
+      },
+    }
+  }
+});
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -9,6 +35,77 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
-    '@nuxt/test-utils'
-  ]
+    '@nuxt/test-utils',
+    '@vite-pwa/nuxt',
+    "@primevue/nuxt-module",
+    "@nuxtjs/tailwindcss",
+  ],
+
+  ssr: true,
+
+  pwa: {
+    registerType: 'autoUpdate',
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module'
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 20
+    },
+    manifest: {
+      name: "Nuxt4 PWA",
+      short_name: "Nuxt4 PWA",
+      description: "Testing Nuxt4 PWA",
+      theme_color: "#10B981",
+      background_color: "#ffffff",
+      display: "standalone",
+      orientation: "portrait",
+      scope: "/",
+      start_url: "/",
+      id: "/",
+      icons: [
+        {
+          src: "icons/nuxt-512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "any maskable"
+        },
+        {
+          src: "icons/nuxt-144.png",
+          sizes: "144x144",
+          type: "image/png"
+        },
+        {
+          src: "icons/nuxt-96.png",
+          sizes: "96x96",
+          type: "image/png"
+        },
+        {
+          src: "icons/nuxt-64.png",
+          sizes: "64x64",
+          type: "image/png"
+        }
+      ]
+    },
+  },
+
+  primevue: {
+    options: {
+      theme: {
+        preset: LaraMountainMeadow,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark-mode',
+          cssLayer: false
+        }
+      }
+    }
+  }
 })
